5e9f95b2ba28b38b86cb708c1f8daa1f0804e568
Kevin-Nengou <kevinnengou02@gmail.com>
2023-09-16 14:19:30 +0100

    Ajout du template dans le projet

--- booking/views.py
+++ booking/views.py
@@ -3,4 +3,17 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
+def faq(request):
+    return render(request, 'faq.html')
+
+def search(request):
+    return render(request, 'search.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+
 # Create your views here.
--- etress/settings.py
+++ etress/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y3mq%*_2tgt90n5za&v0+yg#2z4@!aiw$k!*^7lcd8epcsdvjt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'etress.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'etress',
+        'HOST': 'localhost',
+        'USER': 'kevin',
+        'PASSWORD': 'Kevelino@02'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -105,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Africa/Douala'
 
@@ -118,10 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT  = os.path.join('media')
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
